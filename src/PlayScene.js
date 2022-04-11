58c7a04b3b1383a316ffb27571255d78832cb95e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-04-11 11:14:59 +0100

    feat: add message functionality

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -1,1 +1,15 @@
-function PlayScene() {}
+function PlayScene(game) {
+    this._game = game;
+    this._readyMessage = new ReadyMessage();
+    this._readyMessage.setVisibilityDuration(100);
+}
+
+PlayScene.prototype.tick = function() {
+    this._readyMessage.tick();
+};
+
+}
+
+PlayScene.prototype.getReadyMessage = function() {
+    return this._readyMessage;
+};
