a36e1acf8a206e7d4ca65d21b181be2ecf0a25c7
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-04-25 09:18:28 +0100

    add an empty last frame to Pacman Dies animation

--- spec/PacmanSpec.js
+++ spec/PacmanSpec.js
@@ -944,8 +944,17 @@ describe("When Pacman touches a ghost", function() {
             game.tick();
             expect(pacman.getCurrentFrame()).toEqual('pacman_dies_11');
             game.tick();
-            expect(ghost.isVisible()).toBeFalsy();
             expect(pacman.getCurrentFrame()).toEqual('pacman_dies_11');
+            game.tick();
+            expect(pacman.getCurrentFrame()).toEqual('pacman_dies_12');
+            game.tick();
+            expect(pacman.getCurrentFrame()).toEqual('pacman_dies_12');
+            game.tick();
+            expect(pacman.getCurrentFrame()).toEqual('pacman_dies_12');
+            game.tick();
+            expect(ghost.isVisible()).toBeFalsy();
+            expect(pacman.getCurrentFrame()).toEqual('pacman_dies_12');
+
             game.tick();
             expect(ghost.isVisible()).toBeTruthy();
             expect(pacman.getDeathFrame()).toEqual(-1);
